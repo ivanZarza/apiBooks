0717d2f86581443584d146dd71f3ad49847cc0e5
ivanZarza <ivanzarzaestevez@gmail.com>
2025-03-10 18:08:39 +0100

    vercel1

--- ApiRest/src/database.js
+++ ApiRest/src/database.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql2');
 
+const {
+  DB_HOST = 'localhost',
+  DB_USER = 'root',
+  DB_PASSWORD = '3st3v3Z99!',
+  DB_NAME = 'AppBooks',
+  DB_PORT = 3306,
+} = process.env
+
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '3st3v3Z99!',
-  database: 'AppBooks',
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle : 10,

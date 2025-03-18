1ccd4e0d3e99da2eaa1be95b6c0e3e68bad51e6e
ivanZarza <ivanzarzaestevez@gmail.com>
2025-03-18 16:48:38 +0100

    wip

--- ApiRest/src/routers/books.router.js
+++ ApiRest/src/routers/books.router.js
@@ -3,10 +3,10 @@ const router = Router();
 const booksCtrl = require('../controller/books.controller');
 const verificarToken = require("../helpers/middlewareAutentificacion");
 
-router.get('/books', verificarToken, booksCtrl.getbooks);
-router.post('/books', verificarToken, booksCtrl.postbooks);
-router.put('/books', verificarToken, booksCtrl.putbooks);
-router.delete('/books', verificarToken, booksCtrl.deletebooks);
+router.get('/books', /* verificarToken,  */booksCtrl.getbooks);
+router.post('/books', /* verificarToken,  */booksCtrl.postbooks);
+router.put('/books', /* verificarToken,  */booksCtrl.putbooks);
+router.delete('/books', /* verificarToken,  */booksCtrl.deletebooks);
 
 module.exports = router;
 

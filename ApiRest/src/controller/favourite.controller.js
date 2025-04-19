5ad56c4f04c58874a2be81c051ec89aea37a36f0
ivanZarza <ivanzarzaestevez@gmail.com>
2025-04-19 15:50:34 +0200

    wip

--- ApiRest/src/controller/favourite.controller.js
+++ ApiRest/src/controller/favourite.controller.js
@@ -9,7 +9,7 @@ const getFavourites = async (req, res) => {
       return;
     }
 
-    const sql = 'SELECT * FROM favourite WHERE id_user = ? AND favourite = 1';
+    const sql = 'SELECT * FROM book WHERE id_user = ? AND favourite = 1';
     const [result] = await pool.query(sql, [id_user]);
 
     res.status(200).json({ ok: true, message: 'Éxito!!', data: result });
@@ -19,20 +19,22 @@ const getFavourites = async (req, res) => {
 };
 
 const putFavourites = async (req, res) => {
-  let { id_book, id_user, favourite } = req.body.favourite;
-  console.log('id_user', id_user, 'id_book', id_book, 'favourite', favourite);
+  let { id_book, id_user } = req.body;
+  console.log('id_user', id_user, 'id_book', id_book);
+  
   if (!id_user || !id_book) {
     res.status(400).json({ ok: false, message: 'Faltan datos' });
     return;
   }
+
   try {
-    let sql = 'UPDATE book SET favourite = ? WHERE id_user = ? AND id_book = ?';
-    let result = await pool.query(sql, [favourite, id_user, id_book]);
+    let sql = 'UPDATE book SET favourite = NOT favourite WHERE id_user = ? AND id_book = ?';
+    let [result] = await pool.query(sql, [id_user, id_book]);
 
     res.status(200).json({ ok: true, message: 'Favorito actualizado con éxito', data: result });
   } catch (error) {
     res.status(500).json({ ok: false, message: error.message });
   }
-}
+};
 
 module.exports = { getFavourites, putFavourites };
